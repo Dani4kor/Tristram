3808af0a718e6f0f921b7610b4d4bd65a4679281
Dani4kor <dani4kor@gmail.com>
2017-05-26 16:11:00 +0300

    remove unused imports

--- test-request.py
+++ test-request.py
@@ -3,7 +3,6 @@
 
 import requests
 import pprint
-import os
 
 
 HOST = 'localhost:5001'
@@ -12,6 +11,7 @@ ENTERPOINT = '/api/v1/'
 ENDPOINT = 'pic'
 
 try:
-    pprint.pprint(requests.get('http://' + HOST + ENTERPOINT+ENDPOINT).json())
+    pprint.pprint(requests.get('http://' + HOST +
+                               ENTERPOINT + ENDPOINT).json())
 except Exception as e:
     print str(e) + '\n'
