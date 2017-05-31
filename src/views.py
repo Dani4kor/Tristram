0ae0f7c52de7a55c3bad7f96212cd2b8f1d39e0a
Dani4kor <dani4kor@gmail.com>
2017-05-31 21:56:02 +0300

    update datetime

--- src/views.py
+++ src/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, jsonify
-from datetime import datetime
+from time import time
 from eve_healthcheck import EveHealthCheck
 
 from src import app
@@ -10,11 +10,11 @@ from src import app
 
 @app.route('/api/v1/datetime', methods=['GET'])
 def get_datetime():
-    return jsonify(datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), mimetype='application/json')
+    return jsonify(datetime=time(), mimetype='application/json')
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-EveHealthCheck(app, '/healthcheck')    
+EveHealthCheck(app, '/healthcheck')
