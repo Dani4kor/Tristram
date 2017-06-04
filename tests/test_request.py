a8044cf35f7601f875593662646fce7f36dbb190
Dani <Dani4kor@gmail.com>
2017-06-04 23:01:35 +0300

    Ufix unit endpoint

--- tests/test_request.py
+++ tests/test_request.py
@@ -23,7 +23,8 @@ class GetImageTestCase(unittest.TestCase):
 
     def endpoint(self, resource):
         self.ENTRY_POINT = 'tristram.herokuapp.com'
-        return 'http://%s/api/v1/%s' % (self.ENTRY_POINT if not sys.argv[1:] else sys.argv[1], resource)
+        self.END_POINT = 'api/v1'
+        return 'http://%s/%s/%s' % (self.ENTRY_POINT if not sys.argv[1:] else sys.argv[1], self.END_POINT,resource)
 
     def test_status(self):
         assert self.get_image().status_code == 200
