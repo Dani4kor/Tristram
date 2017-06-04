f7cf24069bf97326916359d3005bda4cb7225ec3
Dani <Dani4kor@gmail.com>
2017-06-04 23:09:46 +0300

    fix p3p8

--- tests/test_request.py
+++ tests/test_request.py
@@ -11,8 +11,7 @@ import unittest
 from pprint import pprint
 
 
-
-class GetImageTestCase(unittest.TestCase):
+class GetListImagesTestCase(unittest.TestCase):
     def get_image(self):
         image = [{}, ]
         return self.perform_get('image', json.dumps(image))
@@ -24,11 +23,11 @@ class GetImageTestCase(unittest.TestCase):
     def endpoint(self, resource):
         self.ENTRY_POINT = 'tristram.herokuapp.com'
         self.END_POINT = 'api/v1'
-        return 'http://%s/%s/%s' % (self.ENTRY_POINT if not sys.argv[1:] else sys.argv[1], self.END_POINT,resource)
+        return 'http://%s/%s/%s' % (self.ENTRY_POINT if not sys.argv[1:] else sys.argv[1], self.END_POINT, resource)
 
     def test_status(self):
         assert self.get_image().status_code == 200
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     unittest.main()
