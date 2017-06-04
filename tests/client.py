17f24b015ae3adcaca1733abbb489d37ff85cc26
Dani <Dani4kor@gmail.com>
2017-06-04 22:39:20 +0300

    add client test

--- tests/client.py
+++ tests/client.py
@@ -1,17 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 
 import requests
+import json
+import random
+
 from pprint import pprint
 
-HOST = 'localhost:5001'
-ENTERPOINT = '/api/v1/'
+ENTRY_POINT = 'tristram.herokuapp.com'
 
-ENDPOINT = 'image'
 
-try:
-    pprint(requests.get('http://' + HOST +
-                        ENTERPOINT + ENDPOINT).json())
-except Exception as e:
-    print(str(e) + '\n')
+def post_image():
+    image = [
+        {
+            'filenmae': 'testName',
+            'tag': 'testNag',
+            'role': ['author'],
+            'image': ['PATH TO PICTURE']
+        },
+    ]
+
+    r = perform_post('image', json.dumps(image))
+    pprint("'image' posted", r.status_code)
+
+    valids = []
+    if r.status_code == 201:
+        response = r.json()
+        if response['_status'] == 'OK':
+            for picture in response['_items']:
+                if picture['_status'] == "OK":
+                    valids.append(picture['_id'])
+
+    return valids
+
+
+def perform_post(resource, data):
+    headers = {'Content-Type': 'application/json'}
+    return requests.post(endpoint(resource), data, headers=headers)
+
+
+def delete():
+    r = perform_delete('image')
+    print("'image' deleted", r.status_code)
+
+
+def perform_delete(resource):
+    return requests.delete(endpoint(resource))
+
+
+def endpoint(resource):
+    return 'http://%s/%s/api/v1' % (ENTRY_POINT if not sys.argv[1:] else sys.argv[1], resource)
+
+
+if __name__ == '__main__':
+    delete()
+    post_image()
