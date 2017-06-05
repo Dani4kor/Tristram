aba33277f7871e547b9244d1a2ce0fa3232147d8
Dani <Dani4kor@gmail.com>
2017-06-05 23:01:44 +0300

    remove test_db

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,16 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pymongo
-
-from pprint import pprint
-from pymongo import MongoClient
-
-# add local mongo or mongodb URL
-client = MongoClient('mongodb://')
-
-db = client.everest
-pic_collection = db.image
-
-cursor = pic_collection.find({})
-pprint([document for document in cursor])
