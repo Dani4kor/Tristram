f82628b039ec358dcd389c6008d8fb80b1a62a1d
Dani <Dani4kor@gmail.com>
2017-09-13 23:01:06 +0300

    update views

--- src/views.py
+++ src/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from time import time
 from eve_healthcheck import EveHealthCheck
 
@@ -12,6 +12,18 @@ from src import app
 def get_datetime():
     return jsonify(datetime=time(), mimetype='application/json')
 
+@app.route('/api/v1/calc', methods=['GET'])
+def get_calc():
+    args =  dict(request.args) 
+    data = ''
+    if 'a' and 'b' in args.keys() and len(args.keys()) == 2: 
+        data = 'success'
+        return jsonify(result = calc(args['a'], args['b']), state=data, mimetype='application/json') 
+    else: 
+        data = 'fail'
+        return jsonify(result = '2 params(a & b) required ', state=data, mimetype='application/json')
+
+def calc(a, b): return int(a[0]) + int(b[0])
 
 @app.route('/')
 def hello_world():
